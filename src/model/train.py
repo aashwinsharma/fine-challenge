69deebfac844cfb9998d32d310c6b0fa2dcbc2a4
Amit Sharma <34742232+aashwinsharma@users.noreply.github.com>
2024-09-13 16:51:11 +0530

    Update train.py

--- src/model/train.py
+++ src/model/train.py
@@ -7,7 +7,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, classification_report
 
-
 import mlflow
 import mlflow.sklearn
 
